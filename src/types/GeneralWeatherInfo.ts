c62ca34757daba75a9e1c3046d67b5f78503d0ec
Vadym Mamchur <vmchr344@gmail.com>
2022-12-26 16:05:58 +0200

    feat: created GeneralWeatherInfo interface

--- src/types/GeneralWeatherInfo.ts
+++ src/types/GeneralWeatherInfo.ts
@@ -1,1 +1,56 @@
-export {};
+export interface GeneralWeatherInfo {
+  coord: WeatherCoord;
+  weather: WeatherInfo[];
+  base: string;
+  main: WeatherMainInfo;
+  visibility: number;
+  wind: WeatherWindInfo;
+  clouds: WeatherCloudsInfo;
+  dt: number;
+  sys: WeatherSys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+interface WeatherCoord {
+  lon: number;
+  lat: number;
+}
+
+interface WeatherInfo {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface WeatherMainInfo {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+  sea_level: number;
+  grnd_level: number;
+}
+
+interface WeatherWindInfo {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface WeatherCloudsInfo {
+  all: number;
+}
+
+interface WeatherSys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
